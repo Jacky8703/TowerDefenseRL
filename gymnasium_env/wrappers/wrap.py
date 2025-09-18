c057d6cffccb014f02e170b52da7f5c3e8f9e6a2
Samuele <corradinsamuele@gmail.com>
2025-09-18 15:53:52 +0200

    minor changes

--- gymnasium_env/wrappers/wrap.py
+++ gymnasium_env/wrappers/wrap.py
@@ -1,7 +1,10 @@
 from gymnasium.wrappers import RecordEpisodeStatistics, RecordVideo, Autoreset
+import datetime
+
+video_id = datetime.datetime.now().strftime("%Y.%m.%d_%H.%M")
 
 def wrap_env(env, episode_recording_gap):
     env = RecordEpisodeStatistics(env)
-    env = RecordVideo(env, video_folder="videos", name_prefix="training", episode_trigger=lambda e: e % episode_recording_gap == 0)
+    env = RecordVideo(env, video_folder=f"videos/{video_id}", name_prefix="training", episode_trigger=lambda e: e % episode_recording_gap == 0)
     env = Autoreset(env)
     return env
