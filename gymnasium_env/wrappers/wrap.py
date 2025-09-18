1e5012e7db629292cff19f04abce2a567eddc365
Samuele <corradinsamuele@gmail.com>
2025-09-18 13:49:57 +0200

    minor changes

--- gymnasium_env/wrappers/wrap.py
+++ gymnasium_env/wrappers/wrap.py
@@ -1,7 +1,7 @@
 from gymnasium.wrappers import RecordEpisodeStatistics, RecordVideo, Autoreset
 
-def wrap_env(env, training_period):
+def wrap_env(env, episode_recording_gap):
     env = RecordEpisodeStatistics(env)
-    env = RecordVideo(env, video_folder="videos", name_prefix="training", episode_trigger=lambda e: e % training_period == 0)
+    env = RecordVideo(env, video_folder="videos", name_prefix="training", episode_trigger=lambda e: e % episode_recording_gap == 0)
     env = Autoreset(env)
     return env
--- train.py
+++ train.py
@@ -1,12 +1,18 @@
+import gymnasium_env.envs  # ensure the custom environment is registered
 import gymnasium as gym
 import logging
 from stable_baselines3.common.callbacks import CheckpointCallback
 from sb3_contrib import MaskablePPO
 from gymnasium_env.wrappers.wrap import wrap_env
 
-# one episode is one game, suppose average game waves = 10, 10 waves = ~300 seconds (27s*wave) and one step = 0.1s in game so 3000 steps per game/episode.
-training_period_episodes = 30 
-training_steps = 100000 # total number of training steps
+mean_time_fps = 250 # mean time/fps from tensor board
+mean_episode_steps = 750 # mean steps per episode from tensor board
+
+hours_to_play = 2
+video_number = 20 # number of videos to record during training
+
+training_steps = mean_time_fps*hours_to_play*3600 # total number of training steps
+episode_recording_gap = (training_steps/mean_episode_steps) // video_number  # one episode = one game
 
 env_name = "gymnasium_env/TowerDefenseWorld-v0"
 
@@ -17,7 +23,7 @@ logging.basicConfig(
 )
 
 env = gym.make(env_name)
-env = wrap_env(env, training_period_episodes)
+env = wrap_env(env, episode_recording_gap)
 
 # save 10 checkpoints
 checkpoint_callback = CheckpointCallback(
@@ -30,15 +36,16 @@ try:
     logging.info(f"--- Starting New Training Run ---")
     logging.info(f"Environment: {env_name}")
     logging.info(f"Total Timesteps: {training_steps} (~{training_steps*0.1/3600:.2f} hours of playing)")
-    logging.info(f"Video Recording Period: {training_period_episodes} games")
-    
+    logging.info(f"Video Recording Period: {episode_recording_gap} games")
+
     model = MaskablePPO("MlpPolicy", env, verbose=1, tensorboard_log="./logs/")
 
     logging.info("Starting model training...")
     model.learn(total_timesteps=training_steps, callback=checkpoint_callback)
-    logging.info("Model training completed.")
-
+    logging.info("Model training completed.")           
+    
     model.save("maskable_ppo_tower_defense")
     logging.info("Model saved.")
 except Exception as e:
     logging.error(f"An error occurred during training: {e}")
+    raise e
